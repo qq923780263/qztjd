881b766aee228d19458e2a9b5072e7abba2a5aa2
qq923780263 <923780263@qq.com>
2023-08-19 18:41:53 +0800

    Add files via upload

---
+++ cocos-js/cc.js
@@ -0,0 +1,98 @@
+System.register(["plugin:cocos/2d.js", "plugin:cocos/sorting.js", "plugin:cocos/3d.js", "./animation.js", "plugin:cocos/audio.js", "plugin:cocos/base.js", "plugin:cocos/gfx-webgl.js", "plugin:cocos/particle.js", "./physics-ammo.js", "./physics-framework.js", "./wait-for-ammo-instantiation.js", "plugin:cocos/primitive.js", "./skeletal-animation.js", "plugin:cocos/tween.js", "plugin:cocos/ui.js"], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [function (_pluginCocos2dJs) {
+      var _exportObj = {};
+      for (var _key in _pluginCocos2dJs) {
+        if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _pluginCocos2dJs[_key];
+      }
+      _export(_exportObj);
+    }, function (_pluginCocosSortingJs) {
+      var _exportObj2 = {};
+      for (var _key2 in _pluginCocosSortingJs) {
+        if (_key2 !== "default" && _key2 !== "__esModule") _exportObj2[_key2] = _pluginCocosSortingJs[_key2];
+      }
+      _export(_exportObj2);
+    }, function (_pluginCocos3dJs) {
+      var _exportObj3 = {};
+      for (var _key3 in _pluginCocos3dJs) {
+        if (_key3 !== "default" && _key3 !== "__esModule") _exportObj3[_key3] = _pluginCocos3dJs[_key3];
+      }
+      _export(_exportObj3);
+    }, function (_animationJs) {
+      var _exportObj4 = {};
+      for (var _key4 in _animationJs) {
+        if (_key4 !== "default" && _key4 !== "__esModule") _exportObj4[_key4] = _animationJs[_key4];
+      }
+      _export(_exportObj4);
+    }, function (_pluginCocosAudioJs) {
+      var _exportObj5 = {};
+      for (var _key5 in _pluginCocosAudioJs) {
+        if (_key5 !== "default" && _key5 !== "__esModule") _exportObj5[_key5] = _pluginCocosAudioJs[_key5];
+      }
+      _export(_exportObj5);
+    }, function (_pluginCocosBaseJs) {
+      var _exportObj6 = {};
+      for (var _key6 in _pluginCocosBaseJs) {
+        if (_key6 !== "default" && _key6 !== "__esModule") _exportObj6[_key6] = _pluginCocosBaseJs[_key6];
+      }
+      _export(_exportObj6);
+    }, function (_pluginCocosGfxWebglJs) {
+      var _exportObj7 = {};
+      for (var _key7 in _pluginCocosGfxWebglJs) {
+        if (_key7 !== "default" && _key7 !== "__esModule") _exportObj7[_key7] = _pluginCocosGfxWebglJs[_key7];
+      }
+      _export(_exportObj7);
+    }, function (_pluginCocosParticleJs) {
+      var _exportObj8 = {};
+      for (var _key8 in _pluginCocosParticleJs) {
+        if (_key8 !== "default" && _key8 !== "__esModule") _exportObj8[_key8] = _pluginCocosParticleJs[_key8];
+      }
+      _export(_exportObj8);
+    }, function (_physicsAmmoJs) {
+      var _exportObj9 = {};
+      for (var _key9 in _physicsAmmoJs) {
+        if (_key9 !== "default" && _key9 !== "__esModule") _exportObj9[_key9] = _physicsAmmoJs[_key9];
+      }
+      _export(_exportObj9);
+    }, function (_physicsFrameworkJs) {
+      var _exportObj10 = {};
+      for (var _key10 in _physicsFrameworkJs) {
+        if (_key10 !== "default" && _key10 !== "__esModule") _exportObj10[_key10] = _physicsFrameworkJs[_key10];
+      }
+      _export(_exportObj10);
+    }, function (_waitForAmmoInstantiationJs) {
+      var _exportObj11 = {};
+      for (var _key11 in _waitForAmmoInstantiationJs) {
+        if (_key11 !== "default" && _key11 !== "__esModule") _exportObj11[_key11] = _waitForAmmoInstantiationJs[_key11];
+      }
+      _export(_exportObj11);
+    }, function (_pluginCocosPrimitiveJs) {
+      var _exportObj12 = {};
+      for (var _key12 in _pluginCocosPrimitiveJs) {
+        if (_key12 !== "default" && _key12 !== "__esModule") _exportObj12[_key12] = _pluginCocosPrimitiveJs[_key12];
+      }
+      _export(_exportObj12);
+    }, function (_skeletalAnimationJs) {
+      var _exportObj13 = {};
+      for (var _key13 in _skeletalAnimationJs) {
+        if (_key13 !== "default" && _key13 !== "__esModule") _exportObj13[_key13] = _skeletalAnimationJs[_key13];
+      }
+      _export(_exportObj13);
+    }, function (_pluginCocosTweenJs) {
+      var _exportObj14 = {};
+      for (var _key14 in _pluginCocosTweenJs) {
+        if (_key14 !== "default" && _key14 !== "__esModule") _exportObj14[_key14] = _pluginCocosTweenJs[_key14];
+      }
+      _export(_exportObj14);
+    }, function (_pluginCocosUiJs) {
+      var _exportObj15 = {};
+      for (var _key15 in _pluginCocosUiJs) {
+        if (_key15 !== "default" && _key15 !== "__esModule") _exportObj15[_key15] = _pluginCocosUiJs[_key15];
+      }
+      _export(_exportObj15);
+    }],
+    execute: function () {}
+  };
+});
